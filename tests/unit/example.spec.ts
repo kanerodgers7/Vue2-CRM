8342d760776373175a4856b80974c2a7b00b747a
kanerodgers7 <jellyfish9629@gmail.com>
2024-09-28 18:25:00 +0900

    feat update config

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
+  it("Renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
